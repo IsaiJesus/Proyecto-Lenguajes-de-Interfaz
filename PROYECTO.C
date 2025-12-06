6efb6a727304c10d4debbe29e4ca8bc4ac8f1e5b
Isai Jesus <isaijesus02@gmail.com>
2025-12-05 19:02:39 -0600

    colisiones

--- PROYECTO.C
+++ PROYECTO.C
@@ -2,6 +2,14 @@
 # include <stdio.h>
 # include <math.h>
 
+void clear(){
+  asm {
+    MOV AH, 0;
+    MOV AL, 3;
+    INT 10H;
+  }
+}
+
 void video(char n){
   asm {
     MOV AH, 0;
@@ -35,13 +43,29 @@ void lineaH(int x, int y, int longitud, char c){
 }
 
 int main(){
+  // Línea horizontal
+  int linY;
+  int j = 0, longitud, lat = 0;
+  // Círculo
   int i, radio = 4, ballX = 0, ballY = 0;
-  int j = 0, longitud = 80, lat = 0;
+  int ar = 1, de = 0, ab = 0, iz = 1;
   video(6);
+
+  printf("Introduce la altura de la linea (1 - 199): ");
+  scanf("%d", &linY);
+  printf("Introduce la longitud de la linea (1 - 640): ");
+  scanf("%d", &longitud);
+  printf("Pulsa cualquier tecla para salir");
+
+  // Borra el enter del printf y espera a que se pulse una tecla
+  fflush(stdin); 
+  getch();
+
+  clear();
 
   while(!kbhit()){
     circulo(ballX, ballY, radio, 0);
-    lineaH(j, 100, longitud, 0);
+    lineaH(j, linY, longitud, 0);
 
     if(lat == 0){
       j++;
@@ -50,10 +74,51 @@ int main(){
       if(j <= 0) lat = 0;
       j--;
     }
-    ballX ++;
-    ballY ++;
+
+    if(ar == 1 && iz == 1){
+      ballX ++;
+      ballY ++;
+    }
+    if(ar == 1 && de == 1){
+      ballX --;
+      ballY ++;
+    }
+    if(ab == 1 && iz == 1){
+      ballX ++;
+      ballY --;
+    }
+    if(ab == 1 && de == 1){
+      ballX --;
+      ballY --;
+    }
+
+    if(ballX + radio == 640){
+      de = 1;
+      iz = 0;
+    }
+    if(ballY + radio == 200){
+      ab = 1;
+      ar = 0;
+    }
+    if(ballX - radio == 0){
+      iz = 1;
+      de = 0;
+    }
+    if(ballY - radio == 0){
+      ar = 1;
+      ab = 0;
+    }
+    // Colisiones con línea horizontal
+    if(ballY - radio == linY && ab == 1 && ballX >= j && ballX <= (j + longitud)){
+      ar = 1;
+      ab = 0;
+    }
+    if(ballY + radio == linY && ar == 1 && ballX >= j && ballX <= (j + longitud)){
+      ar = 0;
+      ab = 1;
+    }
     
-    lineaH(j, 100, longitud, 1);
+    lineaH(j, linY, longitud, 1);
     circulo(ballX, ballY, radio, 1);
   }
 
